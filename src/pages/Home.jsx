239dce7d2e1b734c597427b0d7def9c55d096077
deehombs <deehombs@gmail.com>
2022-09-24 18:44:58 +0100

    set up

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Veg from "../components/Veg";
+import Popular from "../components/Popular";
+
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <Veg/>
+        <Popular/>
+    </div>
+  )
+}
+
+export default Home
